a2d02e5e5a4372219d34fcc49893898fc99cd91d
TAMS demo laptop <tams@tams.informatik.uni-hamburg.de>
2018-09-27 18:14:34 +0200

    Revert "towards python3 support (#1054)"
    This reverts commit 8c62cf20c8c2200d098d55d0e506fbce57ee541d.
    
    Breaks Python2 support

--- moveit_commander/src/moveit_commander/robot.py
+++ moveit_commander/src/moveit_commander/robot.py
@@ -36,7 +36,7 @@ from moveit_commander import MoveGroupCommander, MoveItCommanderException
 from moveit_ros_planning_interface import _moveit_robot_interface
 from moveit_msgs.msg import RobotState
 from visualization_msgs.msg import MarkerArray
-import moveit_commander.conversions as conversions
+import conversions
 
 
 class RobotCommander(object):
